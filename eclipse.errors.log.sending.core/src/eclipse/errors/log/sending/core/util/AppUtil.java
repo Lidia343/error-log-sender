8b942a10451b2423131a9baacb74d57d0625287b
дом <@169.254.170.118>
2020-07-09 14:20:55 +0300

    Создан класс AppUtil. Он содержит методы для проверки того, является ли символ цифрой ("isDigit"), для получения текущих даты и времени ("getCurrentDateAndTime), добавления символа "0" к началу строки, если её длина равна единице ("addZeroToString"), записи элемента в .xml-файл ("writeToXmlFile"), получения в виде строки содержимого входного потока объекта класса ProcessBuilder ("getInputStreamAsString"), записи строки в выходной поток ("writeBytes), записи входного потока в выходной ("writeInputStreamToOutputStream).

---
+++ eclipse.errors.log.sending.core/src/eclipse/errors/log/sending/core/util/AppUtil.java
@@ -0,0 +1,91 @@
+package eclipse.errors.log.sending.core.util;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.zip.ZipOutputStream;
+
+import eclipse.errors.log.sending.core.exceptions.BlockedThreadException;
+
+public class AppUtil 
+{
+	public static boolean isDigit (char c) 
+	{
+		String s = Character.toString(c);
+		try 
+		{
+			Integer.parseInt(s);
+		} 
+		catch (NumberFormatException e) 
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public static String getCurrentDateAndTime ()
+	{
+		Calendar calendar = GregorianCalendar.getInstance();
+		
+		String day = addZeroToString((Integer.toString(calendar.get(Calendar.DAY_OF_MONTH))));
+		String month =  addZeroToString(Integer.toString(calendar.get(Calendar.MONTH) + 1));
+		String year = Integer.toString(calendar.get(Calendar.YEAR));
+		
+		String hour =  addZeroToString(Integer.toString(calendar.get(Calendar.HOUR)));
+		String min =  addZeroToString(Integer.toString(calendar.get(Calendar.MINUTE)));
+		String sec =  addZeroToString(Integer.toString(calendar.get(Calendar.SECOND)));
+		
+		return day + "-" + month + "-" + year + " - " + hour + "-" + min + "-" + sec;
+	}
+	
+	public static String addZeroToString (String a_string)
+	{
+		return a_string.length() == 1 ? "0" + a_string : a_string;
+	}
+	
+	public static void writeToXmlFile (FileWriter a_writer, String a_tag, String a_entry) throws IOException
+	{
+		a_writer.write("\t<" + a_tag + ">");
+		a_writer.write(a_entry);
+		a_writer.write("</" + a_tag + ">\n");
+	}
+	
+	public static String getInputStreamAsString (ProcessBuilder a_processBuilder) throws IOException, InterruptedException, BlockedThreadException
+	{
+		Process process = a_processBuilder.start();
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufReader = new BufferedReader (new InputStreamReader (process.getInputStream()));
+		
+		String line;
+		while ((line = bufReader.readLine()) != null)
+		{
+			stringBuilder.append(line + "\n");
+		}
+		if (process.waitFor() != 0) throw new BlockedThreadException();
+		return stringBuilder.toString();
+	}
+	
+	public static void writeBytes (OutputStream a_out, String a_string) throws IOException
+	{
+		for (int i = 0; i < a_string.length(); i++)
+		{
+			a_out.write((byte)a_string.charAt(i));
+		}
+	}
+	
+	public static void writeInputStreamToOutputStream (FileInputStream a_fis, ZipOutputStream a_zout) throws IOException
+	{
+		byte[] buffer = new byte[64*1024];
+		int length = a_fis.read(buffer);
+		while (length > 0)
+		{
+			a_zout.write(buffer, 0, length);
+			length = a_fis.read(buffer);
+		}
+	}
+}
