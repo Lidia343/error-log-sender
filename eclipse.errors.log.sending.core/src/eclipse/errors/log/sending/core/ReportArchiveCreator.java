7f87d26a19b32e65e7fa942ea2d171e6a0e96bcc
дом <@169.254.170.118>
2020-07-09 14:32:07 +0300

    В классе ReportArchiveCreator откорректированы имена некоторых переменных и методов. Теперь в нём используются методы класса AppUtil.

--- eclipse.errors.log.sending.core/src/eclipse/errors/log/sending/core/ReportArchiveCreator.java
+++ eclipse.errors.log.sending.core/src/eclipse/errors/log/sending/core/ReportArchiveCreator.java
@@ -4,49 +4,24 @@ import org.eclipse.core.runtime.Platform;
 
 import eclipse.errors.log.sending.core.exceptions.BlockedThreadException;
 import eclipse.errors.log.sending.core.system.SystemInformation;
+import eclipse.errors.log.sending.core.util.AppUtil;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class ReportArchiveCreator 
 {
 	private final String m_systemInfFileName = "metadata.xml";
-	private String m_zipFileName = "report";
-	private final String m_zipFileComment = "Archive with system information and the error log.";
+	private String m_reportArchivePath = "report";
 	
 	public ReportArchiveCreator ()
 	{
-		correctZipFileName();
-	}
-	
-	private void correctZipFileName ()
-	{
-		Calendar calendar = GregorianCalendar.getInstance();
-		
-		String day = redactTimePart(Integer.toString(calendar.get(Calendar.DAY_OF_MONTH)));
-		String month =  redactTimePart(Integer.toString(calendar.get(Calendar.MONTH) + 1));
-		String year = Integer.toString(calendar.get(Calendar.YEAR));
-		
-		String hour =  redactTimePart(Integer.toString(calendar.get(Calendar.HOUR)));
-		String min =  redactTimePart(Integer.toString(calendar.get(Calendar.MINUTE)));
-		String sec =  redactTimePart(Integer.toString(calendar.get(Calendar.SECOND)));
-		
-		String now = day + "-" + month + "-" + year + " - " + hour + "-" + min + "-" + sec;
-		
-		m_zipFileName += " (" + now + ").zip";
-	}
-	
-	private String redactTimePart (String a_part)
-	{
-		if (a_part.length() == 1) a_part = "0" + a_part;
-		return a_part;
+		m_reportArchivePath += " (" + AppUtil.getCurrentDateAndTime() + ").zip";
 	}
 	
 	public void createReportArchive () throws IOException, InterruptedException, BlockedThreadException
@@ -55,34 +30,36 @@ public class ReportArchiveCreator
 		String logFilePath = logFile.getPath();
 		int endIndex = logFilePath.lastIndexOf('.');
 		logFilePath = logFilePath.substring(0, endIndex);
+		m_reportArchivePath = logFilePath + m_reportArchivePath;
 		
-		try (FileInputStream logFis = new FileInputStream(logFile); FileOutputStream fout = new FileOutputStream(logFilePath + m_zipFileName);
-			 ZipOutputStream zout = new ZipOutputStream(fout))
+		try (FileInputStream logFin = new FileInputStream(logFile); FileOutputStream logFout = new FileOutputStream(m_reportArchivePath);
+			 ZipOutputStream zipOut = new ZipOutputStream(logFout))
 		{
-			zout.setComment(m_zipFileComment);
-			zout.putNextEntry(new ZipEntry(logFile.getName()));
+			zipOut.putNextEntry(new ZipEntry(logFile.getName()));
 			
-			writeToOutputStream(logFis, zout);
-			zout.closeEntry();
+			AppUtil.writeInputStreamToOutputStream(logFin, zipOut);
+			zipOut.closeEntry();
 			
 			File systemInfFile = new File(m_systemInfFileName);
-			FileWriter systemWriter = new FileWriter(systemInfFile, false);
 			
-			SystemInformation systemInf = new SystemInformation();
-			systemWriter.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-			systemWriter.write("<metadata>\n");
-			writeXmlElement(systemWriter, "osName", systemInf.getOsName());
-			writeXmlElement(systemWriter, "username", systemInf.getUsername());
-			writeXmlElement(systemWriter, "ramAmount", systemInf.getRamAmount());
-			writeXmlElement(systemWriter, "screenResolution", systemInf.getScreenResolution());
-			systemWriter.write("</metadata>");
-			systemWriter.close();
+			try (FileWriter systemInfFileWriter = new FileWriter(systemInfFile, false))
+			{
+				SystemInformation systemInf = new SystemInformation();
+				systemInfFileWriter.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+				systemInfFileWriter.write("<metadata>\n");
+				AppUtil.writeToXmlFile(systemInfFileWriter, "osName", systemInf.getOsName());
+				AppUtil.writeToXmlFile(systemInfFileWriter, "username", systemInf.getUsername());
+				AppUtil.writeToXmlFile(systemInfFileWriter, "ramAmount", systemInf.getRamAmount());
+				AppUtil.writeToXmlFile(systemInfFileWriter, "screenResolution", systemInf.getScreenResolution());
+				systemInfFileWriter.write("</metadata>");
+			}
 			
-			zout.putNextEntry(new ZipEntry(m_systemInfFileName));
+			zipOut.putNextEntry(new ZipEntry(m_systemInfFileName));
 			
-			FileInputStream systemFis = new FileInputStream(systemInfFile);
-			writeToOutputStream(systemFis, zout);
-			systemFis.close();
+			try (FileInputStream systemInfFin = new FileInputStream(systemInfFile))
+			{
+				AppUtil.writeInputStreamToOutputStream(systemInfFin, zipOut);
+			}
 		}
 		catch (IOException e)
 		{
@@ -90,21 +67,8 @@ public class ReportArchiveCreator
 		}
 	}
 	
-	private void writeToOutputStream (FileInputStream a_fis, ZipOutputStream a_zout) throws IOException
+	public String getReportArchiveName ()
 	{
-		byte[] buffer = new byte[64*1024];
-		int length = a_fis.read(buffer);
-		while (length > 0)
-		{
-			a_zout.write(buffer, 0, length);
-			length = a_fis.read(buffer);
-		}
-	}
-	
-	private void writeXmlElement (FileWriter a_writer, String a_tag, String a_entry) throws IOException
-	{
-		a_writer.write("\t<" + a_tag + ">");
-		a_writer.write(a_entry);
-		a_writer.write("</" + a_tag + ">\n");
+		return m_reportArchivePath;
 	}
 }
