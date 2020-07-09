76f644b91d733875e41887909b506192aa389c4f
дом <@169.254.170.118>
2020-07-09 14:24:56 +0300

    В классе SystemInformation теперь используются методы класса AppUtil.

--- eclipse.errors.log.sending.core/src/eclipse/errors/log/sending/core/system/SystemInformation.java
+++ eclipse.errors.log.sending.core/src/eclipse/errors/log/sending/core/system/SystemInformation.java
@@ -1,13 +1,12 @@
 package eclipse.errors.log.sending.core.system;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import org.eclipse.swt.graphics.Device;
 import org.eclipse.swt.widgets.Display;
 
 import eclipse.errors.log.sending.core.exceptions.BlockedThreadException;
+import eclipse.errors.log.sending.core.util.AppUtil;
 
 public class SystemInformation 
 {
@@ -36,12 +35,12 @@ public class SystemInformation
 		if (osName.contains("win"))
 		{
 			processBuilder.command("cmd.exe", "/c", "wmic memorychip get Capacity");
-			String result = getInputStream(processBuilder);
+			String result = AppUtil.getInputStreamAsString(processBuilder);
 			
 			String ramAmountInfo = "";
 			for (char c : result.toCharArray())
 			{
-				if (isDigit(c)) ramAmountInfo += Character.toString(c);
+				if (AppUtil.isDigit(c)) ramAmountInfo += Character.toString(c);
 				else if (!ramAmountInfo.equals(""))
 				{
 					if (!ramAmountInfo.endsWith(" ")) ramAmountInfo += " ";
@@ -65,13 +64,13 @@ public class SystemInformation
 		if (osName.contains("nux"))
 		{
 			processBuilder.command("bash", "-c", "cat /proc/meminfo");
-			String result = getInputStream(processBuilder);
+			String result = AppUtil.getInputStreamAsString(processBuilder);
 			
 			String ramAmountLine = "";
 			int beginIndex = "MemTotal:".length();
 			for (char c : result.substring(beginIndex).toCharArray())
 			{
-				if (isDigit(c)) ramAmountLine += Character.toString(c);
+				if (AppUtil.isDigit(c)) ramAmountLine += Character.toString(c);
 				else if (!ramAmountLine.equals("")) break;
 			}
 			long ramAmount = Math.round((double) (Long.parseLong(ramAmountLine) / Math.pow(1024, 2)));
@@ -79,36 +78,6 @@ public class SystemInformation
 		}
 		return null;
 	}
-	
-	private String getInputStream (ProcessBuilder a_processBuilder) throws IOException, InterruptedException, BlockedThreadException
-	{
-		Process process = a_processBuilder.start();
-		StringBuilder stringBuilder = new StringBuilder();
-		BufferedReader bufReader = new BufferedReader (new InputStreamReader (process.getInputStream()));
-		
-		String line;
-		while ((line = bufReader.readLine()) != null)
-		{
-			stringBuilder.append(line + "\n");
-		}
-		if (process.waitFor() != 0) throw new BlockedThreadException();
-		return stringBuilder.toString();
-	}
-
-	private boolean isDigit (char c) 
-	{
-		String s = Character.toString(c);
-		try 
-		{
-			Integer.parseInt(s);
-		} 
-		catch (NumberFormatException e) 
-		{
-			return false;
-		}
-		return true;
-	}
-	
 	
 	public String getScreenResolution ()
 	{
