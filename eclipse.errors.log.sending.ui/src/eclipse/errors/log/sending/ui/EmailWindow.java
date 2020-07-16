e3faabc4c46a71e2d615b2b54a75856756c6616e
дом <@169.254.170.118>
2020-07-16 20:38:29 +0300

    Завершён интерфейс для ввода пользователем адреса электронной почты. Теперь после нажатия кнопки "Сохранить" адрес сохраняется в папке "eclipse-report-plugin" (в данной директории хранится также файл status.txt с состоянием "открыто"/"закрыто" окна для ввода email-а) во временном каталоге пользователя.

--- eclipse.errors.log.sending.ui/src/eclipse/errors/log/sending/ui/EmailWindow.java
+++ eclipse.errors.log.sending.ui/src/eclipse/errors/log/sending/ui/EmailWindow.java
@@ -1,47 +1,136 @@
 package eclipse.errors.log.sending.ui;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.PlatformUI;
 
-import eclipse.errors.log.sending.core.EmailWorker;
+import eclipse.errors.log.sending.core.email.EmailWorker;
+import eclipse.errors.log.sending.core.email.IEmailSavingListener;
+import eclipse.errors.log.sending.core.util.AppUtil;
 
-public class EmailWindow 
+public class EmailWindow
 {
+	private final String m_openedEmailWindow = "opened";
+	private final String m_closedEmailWindow = "closed";
+	
+	private final String m_statusFilePath = EmailWorker.EMAIL_FILE_PATH_PART + File.separator + "emailWindowStatus.txt";
+	
+	private Shell m_parent;
+	private IEmailSavingListener m_emailSavinglistener;
+	
+	public EmailWindow (IEmailSavingListener a_emailSavinglistener)
+	{
+		m_emailSavinglistener = a_emailSavinglistener;
+	}
+	
 	public void show () throws IOException
 	{
-		EmailWorker emailWorker = new EmailWorker();
-		if (emailWorker.checkEmailFile()) return;
+		String status = AppUtil.readFromFile(m_statusFilePath);
+		if (status != null && status.equals(m_openedEmailWindow)) return;
 		
-		Shell shell = new Shell(Display.getCurrent());
-		shell.setText("Ввод адреса электронной почты");
-		shell.setMinimumSize(325, 75);
+		AppUtil.writeToFile(m_statusFilePath, m_openedEmailWindow);
+		
+		m_parent = new Shell(Display.getCurrent());
+		m_parent.setText("Ввод адреса электронной почты");
+		m_parent.setMinimumSize(325, 107);
+		m_parent.setImage(getShellImage());
 		
 		GridLayout layout = new GridLayout(1, true);
-		shell.setLayout(layout);
+		m_parent.setLayout(layout);
 		
-		Composite composite = new Composite(shell, SWT.BORDER);
+		Composite composite = new Composite(m_parent, SWT.BORDER);
 		composite.setLayout(new GridLayout(2, false));
-		composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		GridData g = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		composite.setLayoutData(g);
 		
 		Label emailLabel = new Label(composite , SWT.NONE);
 		emailLabel.setText("Email: ");
 		emailLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false));
 		
 		Text emailText = new Text(composite , SWT.NONE);
-		emailText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		emailText.setLayoutData(g);
+		
+		Button savingButton = new Button(composite, SWT.PUSH);
+		savingButton.setText("Сохранить");
+		savingButton.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 2, 1));
+		m_parent.setDefaultButton(savingButton);
+		
+		savingButton.addSelectionListener(new SelectionListener()
+		{
+			@Override
+			public void widgetSelected(SelectionEvent a_e) 
+			{
+				String line = emailText.getText();
+				EmailWorker emailWorker = new EmailWorker();
+				if (emailWorker.isEmail(line))
+				{
+					try 
+					{
+						AppUtil.writeToFile(EmailWorker.EMAIL_FILE_PATH, line);
+						MessageDialog.openInformation(m_parent, "Сохранение адреса почты", "Адрес электронной почты сохранён.");
+						m_parent.dispose();
+					} 
+					catch (IOException e)
+					{
+						e.printStackTrace();
+					}
+				}
+				else
+				{
+					MessageDialog.openWarning(m_parent, "Проверка адреса почты", "Некорректный адрес электронной почты.");
+				}
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent a_e) 
+			{
+				
+			}
+		});
+		
+		m_parent.addDisposeListener(new DisposeListener()
+		{
+			@Override
+			public void widgetDisposed(DisposeEvent a_e)
+			{
+				try
+				{
+					AppUtil.writeToFile(m_statusFilePath, m_closedEmailWindow);
+					m_parent.dispose();
+					m_emailSavinglistener.emailSaved();
+				}
+				catch (Exception e) 
+				{
+					MessageDialog.openError(m_parent, "Ошибка", SendCommand.ERROR_MESSAGE + e.getMessage());
+				}
+			}
+		});
 		
 		composite.pack();
-		shell.pack();
-		shell.open();
-		
-		emailWorker.writeInEmailFile("?");
+		m_parent.pack();
+		m_parent.open();
+	}
+	
+	private Image getShellImage ()
+	{
+		ISharedImages sharedImages = PlatformUI.getWorkbench().getSharedImages();
+		return sharedImages.getImageDescriptor(ISharedImages.IMG_OBJ_FILE).createImage();
 	}
 }
--- eclipse.errors.log.sending.ui/src/eclipse/errors/log/sending/ui/SendCommand.java
+++ eclipse.errors.log.sending.ui/src/eclipse/errors/log/sending/ui/SendCommand.java
@@ -9,7 +9,8 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import eclipse.errors.log.sending.core.EmailWorker;
+import eclipse.errors.log.sending.core.email.EmailWorker;
+import eclipse.errors.log.sending.core.email.IEmailSavingListener;
 import eclipse.errors.log.sending.core.ReportArchiveCreator;
 import eclipse.errors.log.sending.core.client.Client;
 
@@ -17,36 +18,51 @@ public class SendCommand extends AbstractHandler
 {
 	private final String m_messageTitle = "Отправка отчёта об ошибке";
 	private final String m_successMessage = "Отчёт об ошибке успешно отправлен";
-	private final String m_errorMessage = "Произошла ошибка. Подробная информация:" + System.lineSeparator();
+	public final static String ERROR_MESSAGE = "Произошла ошибка. Подробная информация:" + System.lineSeparator();
+	
+	private Shell m_parent = null;
+	private Client m_client = null;
 	
 	@Override
 	public Object execute (ExecutionEvent a_event) throws ExecutionException 
 	{
-		Client client = null;
-		Shell parent = HandlerUtil.getActiveShell(a_event);
+		m_parent = HandlerUtil.getActiveShell(a_event);
 		try 
 		{
+			IEmailSavingListener emailSavingListener = new IEmailSavingListener ()
+			{
+				@Override
+				public void emailSaved() throws Exception
+				{
+					sendReportArchive();
+				}
+			};
+			
 			EmailWorker emailWorker = new EmailWorker();
 			if (!emailWorker.checkEmailFile())
 			{
-				EmailWindow emailWindow = new EmailWindow();
+				EmailWindow emailWindow = new EmailWindow(emailSavingListener);
 				emailWindow.show();
 			}
-			
-			ReportArchiveCreator archiveCreator = new ReportArchiveCreator();
-			archiveCreator.createReportArchive();
-			client = new Client();
-			client.sendReportArchive(archiveCreator.getReportArchivePath());
-			MessageDialog.openInformation(parent, m_messageTitle, m_successMessage);
+			else sendReportArchive();
 		} 
 		catch (IOException | InterruptedException e) 
 		{
-			MessageDialog.openError(parent, m_messageTitle, m_errorMessage + e.getMessage());
+			MessageDialog.openError(m_parent, m_messageTitle, ERROR_MESSAGE + e.getMessage());
 		}
 		finally 
 		{
-			if (client != null) client.deleteArchive();
+			if (m_client != null) m_client.deleteArchive();
 		}
 		return null;
+	}
+	
+	private void sendReportArchive() throws IOException, InterruptedException
+	{
+		ReportArchiveCreator archiveCreator = new ReportArchiveCreator();
+		archiveCreator.createReportArchive();
+		m_client = new Client();
+		m_client.sendReportArchive(archiveCreator.getReportArchivePath());
+		MessageDialog.openInformation(m_parent, m_messageTitle, m_successMessage);
 	}
 }
