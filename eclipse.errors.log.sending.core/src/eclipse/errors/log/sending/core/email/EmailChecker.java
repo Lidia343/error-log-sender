e1cef47e1ef99bda028a316fc49c9c44a3185f93
дом <@169.254.170.118>
2020-07-17 22:14:40 +0300

    Класс EmailWorker переименован в EmailChecker. Вместо метода "checkEmailInFile" добавлен метод "checkEmailPreference", проверяющий, устанавливал ли пользователь email в настройках (true) или нет (false).

--- eclipse.errors.log.sending.core/src/eclipse/errors/log/sending/core/email/EmailChecker.java
+++ eclipse.errors.log.sending.core/src/eclipse/errors/log/sending/core/email/EmailChecker.java
@@ -1,32 +1,23 @@
 package eclipse.errors.log.sending.core.email;
 
-import java.io.File;
 import java.io.IOException;
 
 import eclipse.errors.log.sending.core.util.AppUtil;
 
-public class EmailWorker 
+public class EmailChecker 
 {
-	public final static String EMAIL_FILE_PATH_PART = System.getProperty("java.io.tmpdir") + File.separator + "eclipse-report-plugin";
-	public final static String EMAIL_FILE_PATH = EMAIL_FILE_PATH_PART + File.separator + "userEmail.txt";
+	public static final String EMAIL_KEY = "EMAIL";
+	public static final int EMAIL_TEXT_LIMIT = 99;
 	
-	public boolean checkEmailFile () throws IOException
+	public boolean checkEmailPreference () throws IOException
 	{
-		File userDir = new File(EMAIL_FILE_PATH_PART);
-		userDir.mkdir();
-		
-		String line = AppUtil.readFromFile(EMAIL_FILE_PATH);
-		if (!isEmail(line)) return false;
-		
+		if (!checkEmail(AppUtil.getEmailFromPreferences())) return false;
 		return true;
 	}
 	
-	public boolean isEmail (String a_email)
+	public boolean checkEmail (String a_email)
 	{
 		if (a_email == null) return false;
-		
-		int end = a_email.indexOf(File.separator);
-		if (end != -1) a_email = a_email.substring(0, end);
 		
 		int atCharIndex = a_email.indexOf('@');
 		if (atCharIndex == -1 || atCharIndex == 0 || atCharIndex >= (a_email.length() - 3)) return false;
