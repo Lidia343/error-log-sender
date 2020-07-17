b94ff7c5c2e008eed7d862ec96181f4af4810cf4
дом <@169.254.170.118>
2020-07-17 22:35:20 +0300

    К точке расширения org.eclipse.core.runtime.preferences добавлено расширение (initializer), которому соответствует класс EmailPreferenceInitializer. Он необходим для установки значения по умолчанию для email.

---
+++ eclipse.errors.log.sending.ui/src/eclipse/errors/log/sending/ui/email/EmailPreferenceInitializer.java
@@ -0,0 +1,18 @@
+package eclipse.errors.log.sending.ui.email;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+
+import eclipse.errors.log.sending.core.email.EmailChecker;
+import eclipse.errors.log.sending.core.util.AppUtil;
+
+public class EmailPreferenceInitializer extends AbstractPreferenceInitializer 
+{
+	@Override
+	public void initializeDefaultPreferences() 
+	{
+		ScopedPreferenceStore scopedPreferenceStore = new ScopedPreferenceStore(InstanceScope.INSTANCE, AppUtil.PLUGIN_ID);
+		scopedPreferenceStore.setDefault(EmailChecker.EMAIL_KEY, "");
+	}
+}
