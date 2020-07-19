6f747a3203071315392e43f5926303f323656bb0
дом <@169.254.170.118>
2020-07-19 07:27:59 +0300

    Теперь класс EmailPreferencePage отправляет старый и новый почтовые адреса на сервер. Так как метод "propertyChange" слушателя изменения email-а зачастую вызывается несколько раз после нажатия на кнопку "Сохранить", в слушателе организована проверка значений oldValue и newValue события PropertyChangeEvent таким образом, что после установки пользователем нового адреса метод клиента "sendEmail" вызывается лишь один раз. Это достигается с помощью использования поля m_oldValue и класса ValueChangeController, реализующего паттерн "Singleton". Если старое и новое значение адреса почты являются корректными, метод "isChangeForSending" возвращает true; это означает, что адреса можно отправить на сервер. Также метод "isChangeForSending" предотвращает повторную отправку одних и тех же значений.

--- eclipse.errors.log.sending.ui/src/eclipse/errors/log/sending/ui/email/EmailPreferencePage.java
+++ eclipse.errors.log.sending.ui/src/eclipse/errors/log/sending/ui/email/EmailPreferencePage.java
@@ -1,6 +1,7 @@
 package eclipse.errors.log.sending.ui.email;
 
-import org.eclipse.core.runtime.preferences.IScopeContext;
+import java.io.IOException;
+
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.StringFieldEditor;
@@ -11,28 +12,59 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
 import org.osgi.service.prefs.BackingStoreException;
 
+import eclipse.errors.log.sending.core.client.Client;
 import eclipse.errors.log.sending.core.email.EmailChecker;
 import eclipse.errors.log.sending.core.util.AppUtil;
 
 public class EmailPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage 
 {
+	private static class ValueChangeController
+	{
+		private static ValueChangeController instance;
+		
+		private String m_sentEmail = "";
+		
+		private ValueChangeController ()
+		{
+		}
+		
+		public static ValueChangeController getInstance ()
+		{
+			if (instance == null) instance = new ValueChangeController();
+			return instance;
+		}
+		
+		public boolean isChangeForSending (String a_oldValue, String a_newValue) throws IOException
+		{
+			if (m_sentEmail.equals(a_newValue)) return false;
+			
+			m_sentEmail = a_newValue;
+			
+			return true;
+		}
+	}
+	
 	private StringFieldEditor m_stringFieldEditor;
+	
+	private final ValueChangeController m_valueChangeController;
+	
+	private String m_oldValue = "";
 	
 	public EmailPreferencePage() 
 	{
 		super(GRID);
+		m_valueChangeController = ValueChangeController.getInstance();
 	}
-
+	
 	@Override
-	public void init(IWorkbench a_workbench) 
+	public void init (IWorkbench a_workbench) 
 	{
-		IScopeContext context = InstanceScope.INSTANCE;
-		ScopedPreferenceStore scopedPreferenceStore = new ScopedPreferenceStore(context, AppUtil.PLUGIN_ID);
+		ScopedPreferenceStore scopedPreferenceStore = new ScopedPreferenceStore(InstanceScope.INSTANCE, AppUtil.PLUGIN_ID);
 		
 		scopedPreferenceStore.addPropertyChangeListener(new IPropertyChangeListener () 
 		{
 			@Override
-			public void propertyChange(PropertyChangeEvent a_event) 
+			public void propertyChange (PropertyChangeEvent a_event) 
 			{
 				if (a_event.getProperty().equals(EmailChecker.EMAIL_KEY))
 				{
@@ -44,6 +76,7 @@ public class EmailPreferencePage extends FieldEditorPreferencePage implements IW
 					{
 						try
 						{
+							m_oldValue = oldValue;
 							AppUtil.putEmailPreference(oldValue);
 							m_stringFieldEditor.setStringValue(oldValue);
 						}
@@ -52,7 +85,21 @@ public class EmailPreferencePage extends FieldEditorPreferencePage implements IW
 							e.printStackTrace();
 						}
 					}
-				}
+					else if (!newValue.equals(m_oldValue) && (emailChecker.checkEmail(oldValue) || oldValue.equals("")))
+					{
+						try 
+						{
+							if (m_valueChangeController.isChangeForSending(oldValue, newValue))
+							{
+								new Client().sendEmail(oldValue, newValue);
+							}
+						} 
+						catch (IOException e) 
+						{
+							e.printStackTrace();
+						}
+					}
+				}	
 			}
 		});
 		
